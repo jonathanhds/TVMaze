79d6df5d5464712c524c46eacbb0deb89d7d06d7
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2022-11-04 14:28:51 -0300

    Cleanup project

--- TVMaze/TVMazeApp.swift
+++ TVMaze/TVMazeApp.swift
@@ -1,17 +1,10 @@
-//
-//  TVMazeApp.swift
-//  TVMaze
-//
-//  Created by Jonathan Souza on 04/11/22.
-//
-
 import SwiftUI
 
 @main
 struct TVMazeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
