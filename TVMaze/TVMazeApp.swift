b5a18ef9cad1f8cb5e7723b6d224838a31682609
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2022-11-04 14:23:15 -0300

    Initial Commit

---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by Jonathan Souza on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
