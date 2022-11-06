31ba85a4cb145b6c609ee39f60564aa043b2e37e
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2022-11-06 19:22:32 -0300

    More unit tests

--- TVMazeTests/API/APITests.swift
+++ TVMazeTests/API/APITests.swift
@@ -3,11 +3,27 @@ import XCTest
 
 final class APITests: XCTestCase {
     
-    func testIntegrationWorks() async throws {
+    func testFetchShows() async throws {
         let api = API()
         
         let shows = try await api.fetchShows()
         
+        XCTAssertTrue(shows.count > 0)
+    }
+
+    func testSearchShows() async throws {
+        let api = API()
+
+        let shows = try await api.searchShows(query: "Under the Dome")
+
+        XCTAssertTrue(shows.count > 0)
+    }
+
+    func testFetchEpisodes() async throws {
+        let api = API()
+
+        let shows = try await api.fetchEpisodes(forShowId: 1)
+
         XCTAssertTrue(shows.count > 0)
     }
 }
